e3dea6a79aa4fa52d685ac3b5ea6b76db090efba
Maik van der Gaag <maik@familie-vandergaag.nl>
2026-09-07 13:15:19 +0200

    updated

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -97,11 +97,11 @@ PROCESS {
     }
 
     Write-Host "# Publishing the extension to the specified target."
-    & bicep publish-extension --bin-osx-arm64 $root/bin/Release/net9.0/osx-arm64/publish/$ExtensionName `
-      --bin-linux-x64 $root/bin/Release/net9.0/linux-x64/publish/$ExtensionName `
-      --bin-linux-arm64 $root/bin/Release/net9.0/linux-arm64/publish/$ExtensionName `
-      --bin-win-x64 $root/bin/Release/net9.0/win-x64/publish/$ExtensionName.exe `
-      --bin-win-arm64 $root/bin/Release/net9.0/win-arm64/publish/$ExtensionName.exe `
+    & bicep publish-extension --bin-osx-arm64 $root/bin/Release/net10.0/osx-arm64/publish/$ExtensionName `
+      --bin-linux-x64 $root/bin/Release/net10.0/linux-x64/publish/$ExtensionName `
+      --bin-linux-arm64 $root/bin/Release/net10.0/linux-arm64/publish/$ExtensionName `
+      --bin-win-x64 $root/bin/Release/net10.0/win-x64/publish/$ExtensionName.exe `
+      --bin-win-arm64 $root/bin/Release/net10.0/win-arm64/publish/$ExtensionName.exe `
       --target $Target `
       --force
     if ($LASTEXITCODE -ne 0) {
